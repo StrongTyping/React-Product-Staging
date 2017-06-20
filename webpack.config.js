c2e9ec9fad0967de2e4d19349528b368336e5892
StrongStrong <772837122@qq.com>
2017-06-20 10:06:34 +0800

    renovate webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const commonConfig = require('./config/base.js');
 if(env == 'development'){
     let develop = require('./config/develop.js');
     module.exports = webpackMerge(commonConfig,develop);
-
 }else if(env == 'production'){
     let product = require('./config/product.js');
     module.exports = webpackMerge(commonConfig,product);
